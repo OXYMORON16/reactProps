7b8203215ea4aab6820a7cfdd7283efc284267f3
Mr. Coder <aashryashrivastava@gmail.com>
2023-03-07 22:30:08 +0530

    final commit 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,8 @@ function App() {
       phone= "+123 456 789"
       email= "b@beyonce.com"     
       />
-      <Card
-      name= "Jack Bauer"
-      imgURL= "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      phone= "+987 654 321"
-      email= "jack@nowhere.com"
-      />
     </div>
+
   );
 }
 
